6c9df384dc7bf4dbb5e3ae9b67ad5f7de738c7aa
sherrillmix <sherrillmix@users.noreply.github.com>
2018-03-07 22:17:17 -0500

    Adjust file paths

--- speciesArea/readData.R
+++ speciesArea/readData.R
@@ -4,14 +4,14 @@ if(!dir.exists('work'))dir.create('work')
 
 source('readInfo.R')
 
-otu<-read.csv('om.csv.gz',stringsAsFactors=FALSE)
+otu<-read.csv('../om.csv.gz',stringsAsFactors=FALSE)
 rareN<-tapply(otu$count,otu$SampleID,rareEquation,rareN)
 write.csv(cbind(names(rareN),'rare'=rareN),'work/rareN.csv',row.names=FALSE)
 speciesAbund<-tapply(otu$count,otu$SampleID,function(xx)sort(xx[xx>0]))
 save(speciesAbund,file='work/speciesAbund.Rdat')
 
 
-otuFiles<-list.files('validation','^om.*\\.csv\\.gz$',full.names=TRUE)
+otuFiles<-list.files('../validation','^om.*\\.csv\\.gz$',full.names=TRUE)
 otu2<-do.call(rbind,lapply(otuFiles,read.csv,stringsAsFactors=FALSE))
 rareN2<-tapply(otu2$count,otu2$SampleID,rareEquation,rareN)
 write.csv(cbind(names(rareN2),'rare'=rareN2),'work/rareN2.csv',row.names=FALSE)
@@ -19,7 +19,7 @@ speciesAbund2<-tapply(otu2$count,otu2$SampleID,function(xx)sort(xx[xx>0]))
 save(speciesAbund2,file='work/speciesAbund2.Rdat')
 
 
-dada<-read.csv('om.dada.csv.gz',stringsAsFactors=FALSE)
+dada<-read.csv('../om.dada.csv.gz',stringsAsFactors=FALSE)
 dadaRareN<-tapply(dada$count,dada$SampleID,rareEquation,500)
 write.csv(cbind(names(dadaRareN),'rare'=dadaRareN),'work/dadaRareN.csv',row.names=FALSE)
 
